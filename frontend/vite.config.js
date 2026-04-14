800b6ef2bc219306de20cbdf66f8f2640f8e8b0a
Iván Sierra <sierraivan93@gmail.com>
2026-04-14 04:06:43 -0300

    fix: correct input paths in vite.config.js for proper build configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,10 @@ export default defineConfig({
         rolldownOptions: {
             input: {
                 index: resolve(__dirname, "index.html"),
-                home: resolve(__dirname, "src/pages/home/home.html")/* ,
-                login: resolve(dirname, "src/pages/auth/login/login.html"),
-                registro: resolve(dirname, "src/pages/auth/registro/registro.html"),
-                adminHome: resolve(dirname, "src/pages/admin/home/home.html"),
-                clientHome: resolve(dirname, "src/pages/client/home/home.html"), */
+                home: resolve(__dirname, "src/pages/home/home.html") ,
+                login: resolve(__dirname, "src/pages/auth/login/login.html"),
+                register: resolve(__dirname, "src/pages/auth/register/register.html"),
+                admin: resolve(__dirname, "src/pages/admin/admin.html"),
             }
         }
     }
